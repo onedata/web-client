455f850131dd8d3181ea4330c48bade694b29e96
Łukasz Opioła <lukasz.opiola@gmail.com>
2015-11-19 12:54:58 +0100

    VFS-1378 improve some specs

--- src/http_client/hackney_ssl2_transport.erl
+++ src/http_client/hackney_ssl2_transport.erl
@@ -29,13 +29,37 @@
 %%% API
 %%%===================================================================
 
-%% @doc Atoms used to identify messages in {active, once | true} mode.
+%%--------------------------------------------------------------------
+%% @doc
+%% Atoms used to identify messages in {active, once | true} mode.
+%% @end
+%%--------------------------------------------------------------------
+-spec messages(term()) -> {ssl2, ssl2_closed, ssl2_error}.
 messages(_) ->
     {ssl2, ssl2_closed, ssl2_error}.
 
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @equiv connect(Host, Port, Opts, infinity)
+%% @end
+%%--------------------------------------------------------------------
+-spec connect(Host :: list(), Port :: inet:port_number(),
+    Opts :: [http_client:opt()]) ->
+    {ok, Socket :: ssl2:socket()} | {error, Reason :: atom()}.
 connect(Host, Port, Opts) ->
     connect(Host, Port, Opts, infinity).
 
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Opens an ssl connection to Host, Port.
+%% @see ssl2:connect/4
+%% @end
+%%--------------------------------------------------------------------
+-spec connect(Host :: list(), Port :: inet:port_number(),
+    Opts :: [http_client:opt()], Timeout :: timeout()) ->
+    {ok, Socket :: ssl2:socket()} | {error, Reason :: atom()}.
 connect(Host, Port, Opts, Timeout) when is_list(Host), is_integer(Port),
     (Timeout =:= infinity orelse is_integer(Timeout)) ->
     % Accepted opts as in ssl2.erl
@@ -57,57 +81,111 @@ connect(Host, Port, Opts, Timeout) when is_list(Host), is_integer(Port),
     ConnectOpts = hackney_util:filter_options(Opts, AcceptedOpts, BaseOpts),
     ssl2:connect(Host, Port, ConnectOpts, Timeout).
 
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @equiv recv(Socket, Length, infinity)
+%% @end
+%%--------------------------------------------------------------------
+-spec recv(Socket :: ssl2:socket(), Size :: non_neg_integer()) ->
+    {ok, binary()} | {error, Reason :: closed | timeout | atom()}.
 recv(Socket, Length) ->
     recv(Socket, Length, infinity).
 
-%% @doc Receive a packet from a socket in passive mode.`
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Receives a packet from a socket in passive mode.
+%% If the socket is closed, returns {error, closed}.
 %% @see ssl2:recv/3
--spec recv(ssl2:socket(), non_neg_integer(), timeout())
-        -> {ok, any()} | {error, closed | atom()}.
+%% @end
+%%--------------------------------------------------------------------
+-spec recv(Socket :: ssl2:socket(), Size :: non_neg_integer(),
+    Timeout :: timeout()) ->
+    {ok, binary()} | {error, Reason :: closed | timeout | atom()}.
 recv(Socket, Length, Timeout) ->
     ssl2:recv(Socket, Length, Timeout).
 
-%% @doc Send a packet on a socket.
-%% @see ssl2:send/2
--spec send(ssl2:socket(), iolist()) -> ok | {error, atom()}.
-send(Socket, Packet) ->
-    ssl2:send(Socket, Packet).
 
-%% @doc Set one or more options for a socket.
+%%--------------------------------------------------------------------
+%% @doc
+%% Writes Data to Socket.
+%% If the socket is closed, returns {error, closed}.
+%% @see ssl2:send/2
+%% @end
+%%--------------------------------------------------------------------
+-spec send(ssl2:socket(), iolist()) -> ok | {error, atom()}.
+send(Socket, Data) ->
+    ssl2:send(Socket, Data).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Sets options according to Options for the socket Socket.
 %% @see ssl2:setopts/2
--spec setopts(ssl2:socket(), list()) -> ok | {error, atom()}.
+%% @end
+%%--------------------------------------------------------------------
+-spec setopts(Socket :: ssl2:socket(), Opts :: [http_client:opt()]) -> ok.
 setopts(Socket, Opts) ->
     ssl2:setopts(Socket, Opts).
 
-%% @doc Assign a new controlling process <em>Pid</em> to <em>Socket</em>.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Assigns a new controlling process to the socket.
+%% A controlling process receives all messages from the socket.
 %% @see ssl2:controlling_process/2
--spec controlling_process(ssl2:socket(), pid())
-        -> ok | {error, closed | not_owner | atom()}.
+%% @end
+%%--------------------------------------------------------------------
+-spec controlling_process(Socket :: ssl2:socket(),
+    NewControllingProcess :: pid()) -> ok.
 controlling_process(Socket, Pid) ->
     ssl2:controlling_process(Socket, Pid).
 
-%% @doc Return the address and port for the other end of a connection.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Returns the address and port number of the peer.
 %% @see ssl2:peername/1
--spec peername(ssl2:socket())
-        -> {ok, {inet:ip_address(), inet:port_number()}} | {error, atom()}.
+%% @end
+%%--------------------------------------------------------------------
+-spec peername(Socket :: ssl2:socket()) ->
+    {ok, {inet:ip_address(), inet:port_number()}} | {error, Reason :: atom()}.
 peername(Socket) ->
     ssl2:peername(Socket).
 
-%% @doc Close a TCP socket.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Gracefully closes the socket.
 %% @see ssl2:close/1
--spec close(ssl2:socket()) -> ok.
+%% @end
+%%--------------------------------------------------------------------
+-spec close(Socket :: ssl2:socket()) -> ok | {error, Reason :: atom()}.
 close(Socket) ->
     ssl2:close(Socket).
 
-%% @doc Immediately close a socket in one or two directions.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Shuts down the connection in one or two directions.
+%% To be able to handle that the peer has done a shutdown on the write
+%% side, the {exit_on_close, false} option is useful.
 %% @see ssl2:shutdown/2
--spec shutdown(ssl2:socket(), read | write | read_write) -> ok.
+%% @end
+%%--------------------------------------------------------------------
+-spec shutdown(Socket :: ssl2:socket(), Type :: read | write | read_write) ->
+    ok | {error, Reason :: atom()}.
 shutdown(Socket, How) ->
     ssl2:shutdown(Socket, How).
 
-%% @doc Get the local address and port of a socket
+%%--------------------------------------------------------------------
+%% @doc
+%% Returns the address and port number of the socket.
 %% @see ssl2:sockname/1
--spec sockname(ssl2:socket())
-        -> {ok, {inet:ip_address(), inet:port_number()}} | {error, atom()}.
-sockname(Socket) ->
-    ssl2:sockname(Socket).
+%% @end
+%%--------------------------------------------------------------------
+-spec sockname(SocketOrAcceptor :: ssl2:socket() | ssl2:acceptor()) ->
+    {ok, {inet:ip_address(), inet:port_number()}} | {error, Reason :: atom()}.
+sockname(SocketOrAcceptor) ->
+    ssl2:sockname(SocketOrAcceptor).
